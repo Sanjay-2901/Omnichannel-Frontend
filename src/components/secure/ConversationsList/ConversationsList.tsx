8282f504471d3a3a0c85b7e592b51b930f72a19c
Sanjay-2901 <sanjay@yavar.in>
2024-02-02 11:19:08 +0530

    loader added

--- src/components/secure/ConversationsList/ConversationsList.tsx
+++ src/components/secure/ConversationsList/ConversationsList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthContext } from '../../../utils/auth/AuthProvider';
 import { httpRequest } from '../../../utils/axios-utils';
 import { useDashboardContext } from '../../../providers/DashboardProvider';
@@ -9,6 +9,7 @@ import {
 import { HiBars3CenterLeft } from 'react-icons/hi2';
 import { FaSearch } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import { LuInfo } from 'react-icons/lu';
 
 const ConversationsList = () => {
   const dashboardContext = useDashboardContext();
@@ -23,8 +24,10 @@ const ConversationsList = () => {
   } = dashboardContext;
   const navigate = useNavigate();
   const assigneeTypeButtons = ['Mine', 'Unassigned', 'All'];
+  const [isConversationsLoading, setIsConversationsLoading] = useState(false);
 
   useEffect(() => {
+    setIsConversationsLoading(true);
     httpRequest({
       url: `api/v1/accounts/${
         authContext?.getUserDetails().account_id
@@ -44,6 +47,9 @@ const ConversationsList = () => {
       })
       .catch((error) => {
         console.error(error);
+      })
+      .finally(() => {
+        setIsConversationsLoading(false);
       });
   }, [
     dashBoardState.selectedInboxId,
@@ -112,71 +118,83 @@ const ConversationsList = () => {
             })}
           </div>
           <ul className='p-0 m-0 overflow-y-scroll h-full'>
-            {conversationList.length > 0 ? (
-              conversationList.map((conversation: Conversation) => (
-                <li
-                  key={conversation.id}
-                  className={`${
-                    dashBoardState.selectedConversationId === conversation.id &&
-                    'bg-[#26292B]'
-                  } mb-2 cursor-pointer hover:bg-[#26292B] p-2 rounded-md flex flex-row justify-between border-b-2 border-b-[#26292B]`}
-                  onClick={() => {
-                    updateDashboardState((prevState: DashBoardState) => {
-                      return {
-                        ...prevState,
-                        selectedConversationId: conversation.id,
-                      };
-                    });
-                  }}
-                >
-                  <div className='flex flex-row'>
-                    <div className='h-10 w-10 rounded-full bg-[#135899] flex flex-row items-center justify-center mr-3 self-end'>
-                      <h6 className='m-0'>
-                        {conversation.meta.sender.name
-                          .split(' ')
-                          .map((word: string) => {
-                            if (word) {
-                              return word[0].toUpperCase();
-                            }
-                          })
-                          .join('')
-                          .slice(0, 2)}
-                      </h6>
+            {isConversationsLoading && (
+              <div className='center-el'>
+                <div className='spinner-border spinner-border-md'></div>
+              </div>
+            )}
+            {conversationList.length > 0
+              ? conversationList.map((conversation: Conversation) => (
+                  <li
+                    key={conversation.id}
+                    className={`${
+                      dashBoardState.selectedConversationId ===
+                        conversation.id && 'bg-[#26292B]'
+                    } mb-2 cursor-pointer hover:bg-[#26292B] p-2 rounded-md flex flex-row justify-between border-b-2 border-b-[#26292B]`}
+                    onClick={() => {
+                      updateDashboardState((prevState: DashBoardState) => {
+                        return {
+                          ...prevState,
+                          selectedConversationId: conversation.id,
+                        };
+                      });
+                    }}
+                  >
+                    <div className='flex flex-row'>
+                      <div className='h-10 w-10 rounded-full bg-[#135899] flex flex-row items-center justify-center mr-3 self-end'>
+                        <h6 className='m-0'>
+                          {conversation.meta.sender.name
+                            .split(' ')
+                            .map((word: string) => {
+                              if (word) {
+                                return word[0].toUpperCase();
+                              }
+                            })
+                            .join('')
+                            .slice(0, 2)}
+                        </h6>
+                      </div>
+                      <div>
+                        <div className='flex items-center mb-2'>
+                          {getIcons(conversation.meta.channel.slice(9))}
+                          <small className='text-[#787f85] text-xs font-semibold ml-1'>
+                            {getConversationDetails(conversation.id).inbox_name}
+                          </small>
+                        </div>
+                        <h6 className='mb-1'>
+                          {conversation.meta.sender.name}
+                        </h6>
+                        <p className='m-0'>
+                          {conversation?.last_non_activity_message?.content &&
+                            (conversation.last_non_activity_message.content
+                              .length < 45
+                              ? conversation.last_non_activity_message.content
+                              : `${conversation.last_non_activity_message.content.slice(
+                                  0,
+                                  45
+                                )}...`)}
+                        </p>
+                      </div>
                     </div>
-                    <div>
-                      <div className='flex items-center mb-2'>
-                        {getIcons(conversation.meta.channel.slice(9))}
-                        <small className='text-[#787f85] text-xs font-semibold ml-1'>
-                          {getConversationDetails(conversation.id).inbox_name}
+                    {+conversation.messages[0].conversation.unread_count >
+                      0 && (
+                      <div className='rounded-full bg-green-400 h-5 w-5 place-self-end flex flex-row items-center justify-center'>
+                        <small className='text-xs text-black font-semibold'>
+                          {+conversation.messages[0].conversation.unread_count <
+                          9
+                            ? conversation.messages[0].conversation.unread_count
+                            : '9+'}
                         </small>
                       </div>
-                      <h6 className='mb-1'>{conversation.meta.sender.name}</h6>
-                      <p className='m-0'>
-                        {conversation?.last_non_activity_message?.content &&
-                          (conversation.last_non_activity_message.content
-                            .length < 45
-                            ? conversation.last_non_activity_message.content
-                            : `${conversation.last_non_activity_message.content.slice(
-                                0,
-                                45
-                              )}...`)}
-                      </p>
-                    </div>
+                    )}
+                  </li>
+                ))
+              : !isConversationsLoading && (
+                  <div>
+                    <LuInfo className='mx-auto mt-5' size={30} />
+                    <h6 className='mt-3 text-center'>No conversations found</h6>
                   </div>
-                  {+conversation.messages[0].conversation.unread_count > 0 && (
-                    <div className='rounded-full bg-green-400 h-5 w-5 place-self-end flex flex-row items-center justify-center'>
-                      <small className='text-xs text-black font-semibold'>
-                        {+conversation.messages[0].conversation.unread_count < 9
-                          ? conversation.messages[0].conversation.unread_count
-                          : '9+'}
-                      </small>
-                    </div>
-                  )}
-                </li>
-              ))
-            ) : (
-              <h6 className='mt-5 text-center'>No conversations found</h6>
-            )}
+                )}
           </ul>
         </div>
       )}
